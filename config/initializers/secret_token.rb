17febc23e5f4d6262b612369578400fecc797521
Alec Hartman <alechartman@Alec-Hartmans-MacBook-Pro.local>
2010-12-04 23:24:23 -0500

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alectest::Application.config.secret_token = '2a64ca625348a7937fa8e72f917bba35c798d1da5e7e12d2a912abae4d3b0dc90d1117ee8d7d15aa9614db60e58078875325d205a3710e3c943ecfba2cb2cc90'
